71b3f06d9ae2feafa498e79a2ecb52c6e173af97
Sean Del Castillo <seanbdcastillo@gmail.com>
2024-03-11 00:43:03 -0700

    Added Interactable GrabPoint exports

--- addons/nxr/scripts/interactable/Interactable.cs
+++ addons/nxr/scripts/interactable/Interactable.cs
@@ -62,10 +62,14 @@ public partial class Interactable : RigidBody3D
 
 
 	// grab point
-	public Node3D PrimaryGrabPoint;
-	public Node3D SecondaryGrabPoint;	
-	public Transform3D PrimaryGrabPointOffset = new(); 
-	public Transform3D SecondaryGrabPointOffset = new(); 
+	[ExportGroup("Grab Points")]
+	[Export]
+	public Interactable PrimaryGrabPoint { set; get; }
+	[Export]
+	public Interactable SecondaryGrabPoint { set; get; }
+
+	public Transform3D PrimaryGrabPointOffset = new();
+	public Transform3D SecondaryGrabPointOffset = new();
 
 	public Node3D InitParent = null; 
 
@@ -84,15 +88,13 @@ public partial class Interactable : RigidBody3D
 	public delegate void OnFullDroppedEventHandler();
 
 
-    public override void _Ready()
-    {
-		PrimaryGrabPoint  ??= this;
-        SecondaryGrabPoint  ??= this; 
-		InitParent = (Node3D)GetParent(); 
-		InitTransform = Transform; 
-		InitFreeze = Freeze; 
-		InitGlobalTransform = Transform; 
-    }
+	public override void _Ready()
+	{
+		InitParent = (Node3D)GetParent();
+		InitTransform = Transform;
+		InitFreeze = Freeze;
+		InitGlobalTransform = Transform;
+	}
 
     public void Grab(Interactor interactor)
 	{
